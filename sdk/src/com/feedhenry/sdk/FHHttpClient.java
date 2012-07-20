34b80ff4510817ccb5f3a78033d3d4eb5050c3d6
Gareth Murphy <gareth.cpm@gmail.com>
2012-07-20 12:23:45 +0100

    Make FHHttpClient package-private and prevent needless JSONObject creation in post method.

--- sdk/src/com/feedhenry/sdk/FHHttpClient.java
+++ sdk/src/com/feedhenry/sdk/FHHttpClient.java
@@ -1,62 +1,71 @@
 package com.feedhenry.sdk;
 
+import android.util.Log;
 import org.apache.http.entity.StringEntity;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import android.content.Context;
-import android.util.Log;
-
 import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
-public class FHHttpClient {
+/**
+ * The static HTTP client for use solely by the FeedHenry SDK, implemented as
+ * outlined at http://loopj.com/android-async-http/
+ */
+class FHHttpClient {
+	private static AsyncHttpClient mClient = new AsyncHttpClient();
+	static {
+		mClient.setUserAgent("fh_android_sdk");
+	}
 
-  private static AsyncHttpClient mClient = new AsyncHttpClient();
-  private static final String USER_AGENT = "fh_android_sdk";
-  
-  public static void post(String pUrl, JSONObject pParams, FHActCallback pCallback) throws Exception {
-    mClient.setUserAgent(USER_AGENT);
-    StringEntity entity = new StringEntity(new JSONObject().toString());
-    if(null != pParams){
-      entity = new StringEntity(pParams.toString(), "UTF-8");
-    }
-    mClient.post(null, pUrl, entity, "application/json", new FHJsonHttpResponseHandler(pCallback));
-  }
-  
-  static class FHJsonHttpResponseHandler extends JsonHttpResponseHandler {
-    
-    private FHActCallback callback = null;
-    
-    public FHJsonHttpResponseHandler(FHActCallback pCallback){
-      super();
-      callback = pCallback;
-    }
-    
-    @Override
-    public void onSuccess(JSONArray pRes) {
-      Log.d(FH.LOG_TAG, "Got response : " + pRes.toString());
-      if(null != callback){
-        FHResponse fhres = new FHResponse(null, pRes, null, null);
-        callback.success(fhres);
-      }
-    }
-    
-    public void onSuccess(JSONObject pRes){
-      Log.d(FH.LOG_TAG, "Got response : " + pRes.toString());
-      if(null != callback){
-        FHResponse fhres = new FHResponse(pRes,null, null, null);
-        callback.success(fhres);
-      }
-    }
-    
-    public void onFailure(Throwable e, String content){
-      Log.e(FH.LOG_TAG, e.getMessage(), e);
-      if(null != callback){
-        FHResponse fhres = new FHResponse(null, null, e, content);
-        callback.fail(fhres);
-      }
-    }
-  }
+	// We only need to declare a POST function; the FeedHenry act calls make no
+	// use of GET.
+	public static void post(String url, JSONObject params,
+			FHActCallback callback) throws Exception {
+		
+		StringEntity entity;
+		if (params != null) {
+			entity = new StringEntity(params.toString(), "UTF-8");
+		} else {
+			entity = new StringEntity(new JSONObject().toString());
+		}
+		
+		mClient.post(null, url, entity, "application/json",
+				new FHJsonHttpResponseHandler(callback));
+	}
+
+	static class FHJsonHttpResponseHandler extends JsonHttpResponseHandler {
+
+		private FHActCallback callback = null;
+
+		public FHJsonHttpResponseHandler(FHActCallback pCallback) {
+			super();
+			callback = pCallback;
+		}
+
+		@Override
+		public void onSuccess(JSONArray pRes) {
+			Log.d(FH.LOG_TAG, "Got response : " + pRes.toString());
+			if (null != callback) {
+				FHResponse fhres = new FHResponse(null, pRes, null, null);
+				callback.success(fhres);
+			}
+		}
+
+		public void onSuccess(JSONObject pRes) {
+			Log.d(FH.LOG_TAG, "Got response : " + pRes.toString());
+			if (null != callback) {
+				FHResponse fhres = new FHResponse(pRes, null, null, null);
+				callback.success(fhres);
+			}
+		}
+
+		public void onFailure(Throwable e, String content) {
+			Log.e(FH.LOG_TAG, e.getMessage(), e);
+			if (null != callback) {
+				FHResponse fhres = new FHResponse(null, null, e, content);
+				callback.fail(fhres);
+			}
+		}
+	}
 }
