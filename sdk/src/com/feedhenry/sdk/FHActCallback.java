4e3ae93dc5ba155af756de74035d6d1f43d1e405
Wei Li <wei.li@feedhenry.com>
2014-05-08 14:35:31 +0100

    revert changes

--- sdk/src/com/feedhenry/sdk/FHActCallback.java
+++ sdk/src/com/feedhenry/sdk/FHActCallback.java
@@ -1,6 +1,26 @@
 package com.feedhenry.sdk;
 
-@Deprecated
+/**
+ * A FHActCallback will be used to execute code after a FH API request finishes running on a background thread. This will make sure the UI does not freeze.
+ * The {@link #success} and {@link #fail} methods will run on the main UI thread.
+ * You can either implement this interface in your app's own classes or using anonymous inner class. 
+ * For example:
+ * <pre>
+ * {@code
+ * FHActRequest request = FH.buildActRequest("readData", new JSONObject());
+ *   request.executeAsync(new FHActCallback(){
+ *     public void success(FHResponse pResp){
+ *       //process response data
+ *     }
+ *        
+ *     public void fail(FHResponse pResp){
+ *       //process error data
+ *     }
+ * })
+ * }
+ * </pre>
+ *
+ */
 public interface FHActCallback {
 
   /**
