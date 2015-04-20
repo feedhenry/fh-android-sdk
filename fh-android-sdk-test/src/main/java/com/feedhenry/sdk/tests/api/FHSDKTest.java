6c458604d7adb88cd925cdcc6f611648773ca095
Wei Li <wei.li@feedhenry.com>
2015-04-20 20:44:11 +0100

    fix merge conflict

--- fh-android-sdk-test/src/main/java/com/feedhenry/sdk/tests/api/FHSDKTest.java
+++ fh-android-sdk-test/src/main/java/com/feedhenry/sdk/tests/api/FHSDKTest.java
@@ -31,14 +31,14 @@ public class FHSDKTest extends AndroidTestCase {
 
     private MockWebServer mockWebServer = null;
 
-  public void setUp() throws Exception {
-    super.setUp();
-    mockWebServer = new MockWebServer();
-    mockWebServer.play(9000);
-    FH.init(getContext(), null); // this will load fhconfig.local.properties file
-    FH.setLogLevel(FH.LOG_LEVEL_VERBOSE);
-    DataManager.getInstance().save(FHAuthSession.SESSION_TOKEN_KEY, "testsessiontoken");
-  }
+    public void setUp() throws Exception {
+        super.setUp();
+        mockWebServer = new MockWebServer();
+        mockWebServer.play(9000);
+        FH.init(getContext(), null); // this will load fhconfig.local.properties file
+        FH.setLogLevel(FH.LOG_LEVEL_VERBOSE);
+        DataManager.getInstance().save(FHAuthSession.SESSION_TOKEN_KEY, "testsessiontoken");
+    }
 
     public void tearDown() throws Exception {
         super.tearDown();
@@ -119,27 +119,6 @@ public class FHSDKTest extends AndroidTestCase {
         p.put("test", "true");
         makeCloudRequest("GET", null, p, true);
         verifyCloudRequest("/v1/cloud/test?test=true", "GET", null, null);
-    }
-    
-    assertNotNull(resJson);
-    assertEquals("cloud", resJson.getString("type"));
-  }
-  
-  private void verifyCloudRequest(String path, String method, Header[] headers, JSONObject params) throws Exception {
-    RecordedRequest request = mockWebServer.takeRequest();
-    assertEquals(method.toLowerCase(), request.getMethod().toLowerCase());
-    
-    String cuidHeader = request.getHeader("x-fh-cuid");
-    assertEquals(getDeviceId(), cuidHeader);
-
-    String sessionToken = request.getHeader("x-fh-sessiontoken");
-    assertNotNull(sessionToken);
-    
-    if(null != headers){
-      for(int i = 0; i<headers.length;i++){
-        String requestHeaderValue = request.getHeader(headers[i].getName());
-        assertEquals(requestHeaderValue, headers[i].getValue());
-      }
     }
 
     public void testCloudDeleteAsync() throws Exception {
