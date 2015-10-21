86ae04285fc0b9001bc894027e05f07b7be44d75
Summers Pittman <secondsun@gmail.com>
2015-10-21 14:47:18 -0400

    Fixing https://issues.jboss.org/browse/RHMAP-2664

--- fh-android-sdk-test/src/main/java/com/feedhenry/sdk/tests/api/FHAuthRequestTest.java
+++ fh-android-sdk-test/src/main/java/com/feedhenry/sdk/tests/api/FHAuthRequestTest.java
@@ -6,14 +6,21 @@
  */
 package com.feedhenry.sdk.tests.api;
 
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.test.AndroidTestCase;
 import com.feedhenry.sdk.FH;
 import com.feedhenry.sdk.FHActCallback;
 import com.feedhenry.sdk.FHResponse;
 import com.feedhenry.sdk.api.FHAuthRequest;
 import com.feedhenry.sdk.api.FHAuthSession;
+import com.feedhenry.sdk.tests.sync.FHTestUtils;
 import com.squareup.okhttp.mockwebserver.MockResponse;
 import com.squareup.okhttp.mockwebserver.MockWebServer;
+import java.util.concurrent.atomic.AtomicBoolean;
+import org.mockito.Mockito;
 
 public class FHAuthRequestTest extends AndroidTestCase {
 
@@ -22,6 +29,7 @@ public class FHAuthRequestTest extends AndroidTestCase {
     public void setUp() throws Exception {
         mockWebServer = new MockWebServer();
         mockWebServer.play(9000);
+        System.setProperty("dexmaker.dexcache", getContext().getCacheDir().getPath());
         FH.init(getContext(), null);
     }
 
@@ -54,4 +62,30 @@ public class FHAuthRequestTest extends AndroidTestCase {
         });
         assertTrue(FHAuthSession.exists());
     }
+    
+    /**
+     * Seems like OAuth 2 requests from google have an Anchor after result=success
+     */
+    public void testOnReceiveLogsInOAuthRequestWhenSuccessIsFollowedByAHash() {
+        final String googleResponse = "https://testing.feedhenry.me/box/srv/1.1/arm/authCallback?fh_auth_session=j6wnpwpb2xjn2a7quutrxubz&authResponse={\"authToken\":\"testToken\",\"email\":\"TestEmail\",\"family_name\":\"Henry\",\"gender\":\"male\",\"given_name\":\"Feed\",\"hd\":\"feedhenry.com\",\"id\":\"8675309\",\"link\":\"https://plus.google.com/8675309\",\"name\":\"Feed Henry\",\"picture\":\"http://www.feedhenry.com/wp-content/uploads/2015/01/fh-rh-top-logo-sm.png\",\"verified_email\":true}&status=complete&result=success#";
+        FHAuthRequest authRequest = new FHAuthRequest(getContext());
+        final AtomicBoolean success = new AtomicBoolean(false);
+        FHActCallback callback = new FHActCallback() {
+
+            @Override
+            public void success(FHResponse pResponse) {
+                success.set(true);
+            }
+
+            @Override
+            public void fail(FHResponse pResponse) {
+                
+            }
+        };
+        authRequest.setPresentingActivity(Mockito.mock(Context.class));
+        BroadcastReceiver oauth2Receiver = FHTestUtils.instanciatePrivateInnerClass("OAuthURLRedirectReceiver", authRequest, callback);
+        oauth2Receiver.onReceive(null, new Intent().putExtra("url", googleResponse));
+        assertTrue(success.get());
+    }
+    
 }
