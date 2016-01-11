6e15ad0cf01989359a52f7f55f136b81019730b2
Summers Pittman <secondsun@gmail.com>
2016-01-11 13:03:47 -0500

    Removing the need for exceptions by letting the type system do the heavy lifting.

--- fh-android-sdk/src/main/java/com/feedhenry/sdk/sync/FHSyncUtils.java
+++ fh-android-sdk/src/main/java/com/feedhenry/sdk/sync/FHSyncUtils.java
@@ -15,7 +15,10 @@
  */
 package com.feedhenry.sdk.sync;
 
+import android.util.Log;
+import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -28,62 +31,76 @@ public class FHSyncUtils {
     private static final char[] DIGITS = {
         '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
     };
+    private static final String TAG = "FHSyncUtils";
 
-    public static JSONArray sortObj(Object pObject) throws Exception {
+    public static JSONArray sortObj(JSONArray pObject) {
         JSONArray results = new JSONArray();
-        if (pObject instanceof JSONArray) {
-            results = new JSONArray();
-            JSONArray castedObj = (JSONArray) pObject;
-            for (int i = 0, length = castedObj.length(); i < length; i++) {
-                JSONObject obj = new JSONObject();
-                obj.put("key", i + "");
-                Object value = castedObj.get(i);
-                if (value instanceof JSONObject || value instanceof JSONArray) {
-                    obj.put("value", sortObj(value));
-                } else {
-                    obj.put("value", value);
-                }
-                results.put(obj);
+        
+        JSONArray castedObj = (JSONArray) pObject;
+        for (int i = 0, length = castedObj.length(); i < length; i++) {
+            JSONObject obj = new JSONObject();
+            obj.put("key", i + "");
+            Object value = castedObj.get(i);
+            if (value instanceof JSONObject || value instanceof JSONArray) {
+                obj.put("value", sortObj(value));
+            } else {
+                obj.put("value", value);
             }
-        } else if (pObject instanceof JSONObject) {
-            JSONArray keys = ((JSONObject) pObject).names();
-            List<String> sortedKeys = sortNames(keys);
-            for (String sortedKey : sortedKeys) {
-                JSONObject obj = new JSONObject();
-                String key = sortedKey;
-                Object value = ((JSONObject) pObject).get(key);
-                obj.put("key", sortedKey);
-                if (value instanceof JSONObject || value instanceof JSONArray) {
-                    obj.put("value", sortObj(value));
-                } else {
-                    obj.put("value", value);
-                }
-                results.put(obj);
-            }
-        } else {
-            throw new Exception("object is not JSONObject or JSONArray");
+            results.put(obj);
         }
+
         return results;
     }
 
-    public static String generateObjectHash(Object pObject) {
-        String hashValue = "";
-        try {
-            JSONArray sorted = sortObj(pObject);
-            hashValue = generateHash(sorted.toString());
-        } catch (Exception e) {
-            e.printStackTrace();
+    public static JSONArray sortObj(JSONObject pObject) {
+        JSONArray results = new JSONArray();
+        
+        JSONArray keys = pObject.names();
+        List<String> sortedKeys = sortNames(keys);
+        for (String sortedKey : sortedKeys) {
+            JSONObject obj = new JSONObject();
+            String key = sortedKey;
+            Object value = ((JSONObject) pObject).get(key);
+            obj.put("key", sortedKey);
+            if (value instanceof JSONObject || value instanceof JSONArray) {
+                obj.put("value", sortObj(value));
+            } else {
+                obj.put("value", value);
+            }
+            results.put(obj);
         }
+        
+        return results;
+    }
+     
+    public static String generateObjectHash(JSONArray pObject) {
+        String hashValue = "";
+        JSONArray sorted = sortObj(pObject);
+        hashValue = generateHash(sorted.toString());
+
         return hashValue;
     }
 
-    public static String generateHash(String pText) throws Exception {
-        String hashValue;
-        MessageDigest md = MessageDigest.getInstance("SHA-1");
-        md.reset();
-        md.update(pText.getBytes("ASCII"));
-        hashValue = encodeHex(md.digest());
+       public static String generateObjectHash(JSONObject pObject) {
+        String hashValue = "";
+        JSONArray sorted = sortObj(pObject);
+        hashValue = generateHash(sorted.toString());
+
         return hashValue;
+    }
+    
+    public static String generateHash(String pText) {
+        try {
+            String hashValue;
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            md.reset();
+            md.update(pText.getBytes("ASCII"));
+            hashValue = encodeHex(md.digest());
+            return hashValue;
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException ex) {
+            Log.e(TAG, ex.getMessage(), ex);
+            throw new RuntimeException(ex);
+        }
     }
 
     private static String encodeHex(byte[] pData) {
@@ -100,17 +117,29 @@ public class FHSyncUtils {
         return new String(out);
     }
 
+    private static Object sortObj(Object value) {
+        if (value instanceof JSONArray) {
+            return sortObj((JSONArray)value);
+        } else if (value instanceof JSONObject) {
+            return sortObj((JSONObject)value);
+        } else {
+            throw new IllegalArgumentException(String.format("A object %s was snuck into a JSON tree", value.toString()));
+        }
+    }
+    
     private static List<String> sortNames(JSONArray pNames) throws JSONException {
         if (pNames == null) {
             return Collections.emptyList();
         }
         
         int length = pNames.length();
-        ArrayList<String> names = new ArrayList<String>(length);
+        ArrayList<String> names = new ArrayList<>(length);
         for (int i = 0; i < length; i++) {
             names.add(pNames.getString(i));
         }
         Collections.sort(names);
         return names;
     }
+
+    
 }
